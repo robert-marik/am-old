70f9afd316de8ad44faae5155e40274c98d7b414
robert-marik <marik@mendelu.cz>
2021-02-02 21:26:12 +0100

    cheatsheet

--- secist_delku_videi.sh
+++ secist_delku_videi.sh
@@ -1,14 +1,20 @@
 l=0
-for i in `grep ^https://youtu.be $1 `
+for i in `grep -h ^https://youtu.be $1 `
 do
-    temp=`youtube-dl $i  --get-duration`
+    cislo=`echo $i | cut -d/ -f4 `
+    if  [ ! -f cache/$cislo.txt ]; then
+	echo "Getting information about $i"
+	temp=`youtube-dl $i  --get-duration`
+	echo $temp > cache/$cislo.txt
+    fi
+    temp=`cat cache/$cislo.txt`
     tempM=`echo $temp |  cut -d: -f1`
     tempS=`echo $temp |  cut -d: -f2`
     l=$((tempM*60+tempS+l))
-    echo "$i: delka $temp"
+    echo "$i $temp min."
 done
 
 l=$((l/60))
 
-echo $1:$l
+echo "$1 total: $l minut"
 
